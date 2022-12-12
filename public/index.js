d8ea372f3c83abced74d1e7599bfe73d6e782ed9
Jose <gldmz0318@icloud.com>
2022-12-12 00:13:57 -0800

    fixed admin.js file

--- public/index.js
+++ public/index.js
@@ -11,18 +11,18 @@ async function main() {
 
     books.forEach(renderBook)
 
-    let responsePatch = await fetch ('http://localHost:9001/updateBook', {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "id": 3,
-            "title": "Legends of Arathrae",
-        }),
-    });
-    let updatedBook = await responsePatch.json();
-    console.log(updatedBook)
+    // let responsePatch = await fetch ('http://localHost:9001/updateBook', {
+    //     method: "PATCH",
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         "id": 3,
+    //         "title": "Legends of Arathrae",
+    //     }),
+    // });
+    // let updatedBook = await responsePatch.json();
+    // console.log(updatedBook)
 }
 
 function renderBook(book) {
